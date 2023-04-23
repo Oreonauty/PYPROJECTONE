a0c37cc740cf0b41ed20951e32ae767f7ab75f83
Oreonauty <emilieee.schmidt@gmail.com>
2023-04-23 19:34:41 +0200

    startet the game. i have a board and a figure. moving is not working

--- main.py
+++ main.py
@@ -1,24 +1,89 @@
 import os
-import numpy as np
+#import numpy as np
+
+#####  BOARD  #######
+
+board_length = 25
+board_heigth = 20
+
+board = [[' ' for _ in range(board_length)] for _ in range(board_heigth)]
+
+board[0] = ['X' for _ in range(board_length)]
+board[board_heigth - 1] = ['X' for _ in range(board_length)]
+for row in board:
+    row[0] = 'X'
+    row[board_length - 1] = 'X'
 
 
-arr_field = np.chararray((15,20))
+#####  FIGURE  #######
 
-arr_field[:,:] = ''
-arr_field[0,:] = '++'
-arr_field[14,:] = '_+'
+pos_fig_head_start = [5,5]
+
+def set_figure(pos_fig_head):
+    
+    board[pos_fig_head[0]] [pos_fig_head[1]] = 'O'
+
+    board[pos_fig_head[0]+1] [pos_fig_head[1]-1] = '/'
+    board[pos_fig_head[0]+1] [pos_fig_head[1]] = '|'
+    board[pos_fig_head[0]+1] [pos_fig_head[1]+1] = '\\'
+
+    board[pos_fig_head[0]+2] [pos_fig_head[1]-1] = '/'
+    board[pos_fig_head[0]+2] [pos_fig_head[1]] = ' '
+    board[pos_fig_head[0]+2] [pos_fig_head[1]+1] = '\\'
+    
+def erase_figure(pos_fig_head):
+    
+    board[pos_fig_head[0]] [pos_fig_head[1]] = ' '
+
+    board[pos_fig_head[0]+1] [pos_fig_head[1]-1] = ' '
+    board[pos_fig_head[0]+1] [pos_fig_head[1]] = ' '
+    board[pos_fig_head[0]+1] [pos_fig_head[1]+1] = ' '
+
+    board[pos_fig_head[0]+2] [pos_fig_head[1]-1] = ' '
+    board[pos_fig_head[0]+2] [pos_fig_head[1]] = ' '
+    board[pos_fig_head[0]+2] [pos_fig_head[1]+1] = ' '    
+
+#####  MOVE  #######
+
+def move_fig(pos_fig_head):
+    new_pos_fig_head = [pos_fig_head[0],pos_fig_head[1]]
+    print('\n To move the figure press: \n \n a --> left \n d --> right \n w --> up \n s --> down \n ')
+    player_move = input()
+    if player_move == 'q':
+        return 'q'
+    if player_move == 'a':
+        new_pos_fig_head[1] = pos_fig_head[1] - 1
+    if player_move == 'd':
+        new_pos_fig_head[1] = pos_fig_head[1] + 1
+    if player_move == 'w':
+        new_pos_fig_head[0] = pos_fig_head[1] - 1
+    if player_move == 's':
+        new_pos_fig_head[0] = pos_fig_head[1] + 1
+    return new_pos_fig_head
+        
+    
+    
 
 
-def drawField(field):
+def drawField(field, pos_fig_head, new_pos_fig_head):
     os.system('cls')
+    erase_figure(pos_fig_head)
+    set_figure(new_pos_fig_head)
     
     for row in field:
-        temp = ''
-        for val in row:
-            temp += str(val)
-        print (temp)
+        print(*row)
 
        
-    
-drawField(arr_field)
-#print(arr_field)
+###########################################################
+##      GAMEPLAY                                         ##
+###########################################################
+pos_fig_head = pos_fig_head_start    
+drawField(board, pos_fig_head = pos_fig_head_start, new_pos_fig_head = pos_fig_head_start)
+playing = True
+while playing == True:
+    new_pos_fig_head = move_fig(pos_fig_head_start)
+    if new_pos_fig_head == 'q':
+        playing = False
+    else:
+        drawField(board, pos_fig_head, new_pos_fig_head)
+
